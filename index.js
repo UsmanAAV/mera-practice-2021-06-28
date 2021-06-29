2cba374ed114b12a0f4e03789e7abbae41bc77b3
Ibragimov Timur <i.timur@mail.ru>
2021-06-29 11:04:19 +0300

    fix bug

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 function check(num) {
-    let checked = true
-    for (let i=2; i <= Math.sqrt(num) && checked; i++) {
-        if (num % i == 0) {
-            checked = false
-        }
+  let checked = true
+  if (num === 1) {
+    console.log(num, "No")
+    return
+  }
+  for (let i = 2; i <= Math.sqrt(num) && checked; i++) {
+    if (num % i == 0) {
+      checked = false
     }
-    if (checked) {
-        console.log(num, "Yes")
-    } else {
-        console.log(num, "No")
-    }
+  }
+  if (checked) {
+    console.log(num, "Yes")
+  } else {
+    console.log(num, "No")
+  }
 }
 
 check(1)
