6a835d3fc1fb2d1858ba12670d16057c5c6afda9
Ibragimov Timur <i.timur@mail.ru>
2021-06-29 11:10:26 +0300

    pure function

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 function check(num) {
   let checked = true
   if (num === 1) {
-    console.log(num, "No")
-    return
+    return false
   }
   for (let i = 2; i <= Math.sqrt(num) && checked; i++) {
     if (num % i == 0) {
       checked = false
     }
   }
-  if (checked) {
-    console.log(num, "Yes")
-  } else {
-    console.log(num, "No")
-  }
+  return checked
 }
 
-check(1)
-check(10)
-check(13)
-check(2)
-check(3)
-check(4)
-check(5)
+console.log(check(1) ? 'Yes' : 'No')
+console.log(check(2) ? 'Yes' : 'No')
+console.log(check(3) ? 'Yes' : 'No')
+console.log(check(4) ? 'Yes' : 'No')
+console.log(check(5) ? 'Yes' : 'No')
