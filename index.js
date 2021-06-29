2c60466d17b1b89a4e2aa0ef4e99559b4610513b
Giknyx <bexultanovsultan@gmail.com>
2021-06-29 13:58:30 +0600

    test1

---
+++ index.js
@@ -0,0 +1,21 @@
+function check(num) {
+    let checked = true
+    for (let i=2; i <= Math.sqrt(num) && checked; i++) {
+        if (num % i == 0) {
+            checked = false
+        }
+    }
+    if (checked) {
+        console.log(num, "Yes")
+    } else {
+        console.log(num, "No")
+    }
+}
+
+check(1)
+check(10)
+check(13)
+check(2)
+check(3)
+check(4)
+check(5)
